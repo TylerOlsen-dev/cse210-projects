dcccca07133030b82f238f0efee7e461292b2714
Tyler Olsen <chilledfisher@gmail.com>
2025-01-16 15:37:58 -0700

    Prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int myNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != myNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (myNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (myNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the number!!");
+            }
+
+        }                    
     }
 }
