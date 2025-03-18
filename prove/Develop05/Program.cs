92964ebd9f4fbca3d5ec62373d0ebf130cd5e8f3
Tyler Olsen <88458473+TylerOlsen-dev@users.noreply.github.com>
2025-03-18 16:49:06 -0600

    Deleted something that was not needed.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
-using System.IO;
-
 namespace goal_assignment
 {
     public class Program
