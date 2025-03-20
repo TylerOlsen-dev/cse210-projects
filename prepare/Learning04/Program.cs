f2d66f9e61dcdb72a9a04bab0d7323a0873b84f3
Tyler Olsen <88458473+TylerOlsen-dev@users.noreply.github.com>
2025-03-19 18:22:59 -0600

    Did Learning activity 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,74 @@
 using System;
 
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+}
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
+
+public class WritingAssignment : Assignment
+{
+    private string _assignmentTitle;
+
+    public WritingAssignment(string studentName, string topic, string assignmentTitle)
+        : base(studentName, topic)
+    {
+        _assignmentTitle = assignmentTitle;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_assignmentTitle} by {GetStudentName()}";
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("John Smith", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("John Smith", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Jane Smith", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
